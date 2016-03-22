42c78d652ce99868f5b079d78914b808b73ad5b7
iamthtm <p_1624_n@hotmail.com>
2016-03-22 22:18:23 +0700

    countbook

--- public/app.js
+++ public/app.js
@@ -3,4 +3,32 @@ angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var todoList = this
     todoList.test = 'ping'
+    todoList.sumbook = []
+    todoList.add = function (id, name, price) {
+      // console.log(id, name, price)
+      var book = {
+        'id': id,
+        'name': name,
+        'price': price
+      }
+      // todoList.sumbook.push(book)
+      // console.log(todoList.sumbook)
+      // นับจำนวนหนังสือที่ซ้ำกัน
+      if (todoList.sumbook.length === 0) {
+        book.countbook = 1
+        todoList.sumbook.push(book)
+      } else {
+        var checkbook = false
+        for (var i = 0; i < todoList.sumbook.length; i++) {
+          if (todoList.sumbook[i].id === id) {
+            checkbook = true
+            todoList.sumbook[i].countbook += 1
+          }
+        }
+      }
+      if (checkbook === false) {
+        book.countbook = 1
+        todoList.sumbook.push(book)
+      }
+    }
   })
