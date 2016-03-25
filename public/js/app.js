e3c9bdb14b0a5a6fa58830b4d981867547678d74
iamthtm <p_1624_n@hotmail.com>
2016-03-25 20:22:20 +0700

    amount

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,9 @@ angular.module('todoApp', [])
     var todoList = this
     todoList.test = 'ping'
     todoList.sumbook = []
+    todoList.discountTotal = 0
+    todoList.bookDiscountTotal = 0
+    todoList.showTotal = 0
     todoList.add = function (id, name, price) {
       // console.log(id, name, price)
       console.log(todoList.sumbook)
@@ -31,5 +34,37 @@ angular.module('todoApp', [])
         book.countbook = 1
         todoList.sumbook.push(book)
       }
+      var bookDiscount = todoList.sumbook
+      todoList.bookDiscountTotal = todoList.discount(bookDiscount)
+      // เรียกfunctionมาแสดงค่า
+      todoList.showTotal = todoList.TotalPrice()
+    }
+    // คำนวณส่วนลด
+    var filterData = function (array) {
+      return array.filter((element) => element.countbook !== 0)
+    }
+    // function Discount
+    todoList.discount = function (book) {
+      var items = book.map((obj) => {
+        return { countbook: obj.countbook, price: obj.price }
+      })
+      var totalDis = 0
+      while (items.length > 1) {
+        var sumprice = items.reduce((sum, item) => sum + item.price, 0)
+        totalDis += ((items.length - 1) / 10) * sumprice
+        items = items.map((obj) => {
+          return { countbook: obj.countbook - 1, price: obj.price }
+        })
+        items = filterData(items)
+      }
+      return totalDis
+    }
+    todoList.TotalPrice = function () {
+      this.amountPrice = 0
+      for (var i = 0; i < todoList.sumbook.length; i++) {
+        this.amountPrice += todoList.sumbook[i].price * todoList.sumbook[i].countbook
+      }
+      return (this.amountPrice)
+    // console.log(this.amountPrice)
     }
   })
