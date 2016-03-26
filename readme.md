2d7146d6a42ef22bccc3a30b2de13a7b37b1e312
iamthtm <p_1624_n@hotmail.com>
2016-03-26 16:08:01 +0700

    edit test & readme

--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 
-##how to install
+##How to install
 ```
-$ npm install
+$ sudo npm install
 ```
-
-Heroku => https://fast-citadel-32622.herokuapp.com/
+##How to start server
+```
+$ sudo npm start
+```
+##How to test
+```
+$ sudo npm test
+```
+##Heroku =>
+```
+https://fast-citadel-32622.herokuapp.com/
+```
--- test/index.js
+++ test/index.js
@@ -25,47 +25,47 @@ describe('TEST Buy Harry Potter Book', function () {
     afterEach(function * () {
       yield nightmare.end()
     })
-    it('ซื้อเล่ม 1 จำนวน 2 เล่ม, ซื้อเล่ม 2 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 20', function * () {
-      var case1 = yield nightmare
-        .goto('http://localhost:5000')
-        .wait(2000)
-        .click('#b1')
-        .click('#b1')
-        .wait(1000)
-        .click('#b2')
-        .evaluate(function () {
-          this.price = document.querySelector('#discountTotal').innerHTML
-          return this.price.substr(1, this.price.length - 1)
-        })
-      case1.should.equal('20')
-    })
-    it('ซื้อเล่ม 2 จำนวน 3 เล่ม, ซื้อเล่ม 3 จำนวน 3 เล่ม ส่วนลดต้องเท่ากับ 60', function * () {
+    it('ซื้อHarry Potterเล่ม 1 จำนวน 2 เล่ม,ซื้อHarry Potterเล่ม 2 จำนวน 1 เล่ม,ซื้อHarry Potterเล่ม 3 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 60', function * () {
       var case2 = yield nightmare
         .goto('http://localhost:5000')
         .wait(2000)
-        .click('#b2')
-        .click('#b2')
+        .click('#b1')
+        .click('#b1')
+        .wait(1000)
         .click('#b2')
         .wait(1000)
         .click('#b3')
-        .click('#b3')
-        .click('#b3')
-        .wait(1000)
         .evaluate(function () {
           this.price = document.querySelector('#discountTotal').innerHTML
           return this.price.substr(1, this.price.length - 1)
         })
       case2.should.equal('60')
     })
-    it('ซื้อเล่ม 3 จำนวน 5 เล่ม, ซื้อเล่ม 4 จำนวน 4 เล่ม, เล่ม 5 จำนวน 3 ส่วนลดต้องเท่ากับ 200', function * () {
+    it('ซื้อHarry Potterเล่ม 1 จำนวน 1 เล่ม,ซื้อHarry Potterเล่ม 2 จำนวน 1 เล่ม,ซื้อHarry Potterเล่ม 3 จำนวน 2 เล่ม,ซื้อHarry Potterเล่ม 4 จำนวน 1 เล่มเล่มส่วนลดต้องเท่ากับ 120', function * () {
+      var case1 = yield nightmare
+        .goto('http://localhost:5000')
+        .wait(2000)
+        .click('#b1')
+        .wait(1000)
+        .click('#b2')
+        .wait(1000)
+        .click('#b3')
+        .click('#b3')
+        .wait(1000)
+        .click('#b4')
+        .evaluate(function () {
+          this.price = document.querySelector('#discountTotal').innerHTML
+          return this.price.substr(1, this.price.length - 1)
+        })
+      case1.should.equal('120')
+    })
+    it('ซื้อHarry Potterเล่ม 3 จำนวน 3 เล่ม,ซื้อHarry Potterเล่ม 4 จำนวน 4 เล่ม,ซื้อHarry Potterเล่ม 5 จำนวน 4 เล่ม,ซื้อHarry Potterเล่ม 6 จำนวน 1 เล่มส่วนลดต้องเท่ากับ 260', function * () {
       var case3 = yield nightmare
         .goto('http://localhost:5000')
         .wait(2000)
         .click('#b3')
         .click('#b3')
         .click('#b3')
-        .click('#b3')
-        .click('#b3')
         .wait(1000)
         .click('#b4')
         .click('#b4')
@@ -75,18 +75,24 @@ describe('TEST Buy Harry Potter Book', function () {
         .click('#b5')
         .click('#b5')
         .click('#b5')
+        .click('#b5')
+        .wait(1000)
+        .click('#b6')
         .evaluate(function () {
           this.price = document.querySelector('#discountTotal').innerHTML
           return this.price.substr(1, this.price.length - 1)
         })
-      case3.should.equal('200')
+      case3.should.equal('260')
     })
-    it('ซื้อเล่ม 6 จำนวน 5 เล่ม, ซื้อเล่ม 7 จำนวน 4 เล่ม, ซื้อเล่ม 2 จำนวน 2 เล่ม, ซื้อเล่ม 3 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 200', function * () {
+    it('ซื้อHarry Potterเล่ม 5 จำนวน 4 เล่ม,ซื้อHarry Potterเล่ม 6 จำนวน 3 เล่ม,ซื้อHarry Potterเล่ม 7 จำนวน 6 เล่ม ส่วนลดต้องเท่ากับ 200', function * () {
       var case4 = yield nightmare
         .goto('http://localhost:5000')
         .wait(2000)
-        .click('#b6')
-        .click('#b6')
+        .click('#b5')
+        .click('#b5')
+        .click('#b5')
+        .click('#b5')
+        .wait(1000)
         .click('#b6')
         .click('#b6')
         .click('#b6')
@@ -95,16 +101,13 @@ describe('TEST Buy Harry Potter Book', function () {
         .click('#b7')
         .click('#b7')
         .click('#b7')
-        .wait(1000)
-        .click('#b2')
-        .click('#b2')
-        .wait(1000)
-        .click('#b1')
+        .click('#b7')
+        .click('#b7')
         .evaluate(function () {
           this.price = document.querySelector('#discountTotal').innerHTML
           return this.price.substr(1, this.price.length - 1)
         })
-      case4.should.equal('220')
+      case4.should.equal('200')
     })
   })
 })
